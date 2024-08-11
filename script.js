b0907ecfd1159a045723abb1f5e34e2e86a4ae18
Ayushi Malviya <147861181+laadliayushi@users.noreply.github.com>
2024-08-11 17:32:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let score = 33
+console.log(typeof score);
+
+let scor = "44"
+console.log(typeof scor);
+
+let mark = Number(scor)    /*here convert string in the form of number*/
+console.log(mark);
+
+let num = "232as"
+console.log(typeof num);
+let number = Number(num)
+console.log(number);    /*here convert value into a NaN (not a number )but type of  also showing number*/
+console.log(typeof number);
+
+
+// let isAyushi = " "                    /*here  type of showing string and boolean type of true */
+console.log(typeof isAyushi);
+let isName = Boolean(isAyushi)
+console.log(isTrue);
+console.log(typeof isTrue);
+
+
+let isAyushi = ""                      /*here  type of showing string and boolean type of false */
+console.log(typeof isAyushi);
+let isTrue = Boolean(isAyushi)
+console.log(isTrue);
+console.log(typeof isTrue);
