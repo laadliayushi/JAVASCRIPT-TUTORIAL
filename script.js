2f7dab7699abb0904961dfa90857e969091787f2
Ayushi Malviya <147861181+laadliayushi@users.noreply.github.com>
2024-08-11 17:37:32 +0530

    Variables

--- script.js
+++ script.js
@@ -1,30 +1,13 @@
-//   this is string detatype
-let name = "ayushi malviya"
-console.log(name);
-// this is number datatype
-let age = 23
-console.log(age);
-// this is boolean datatype
-let isReal = true
-console.log(isreal);
+const acoountId = 23434;
+var accountName = "ayushi";
+let accountPassword = "malviya";
+aacountCity = "bhopla"
+console.log(acoountId);
+console.log(accountName);
+console.log(accountPassword);
+console.log(typeof aacountCity);
+// acoountId = 2 not Allowe to change const "const is constent always"
+accountName = "ojsdh"
+accountPassword = "ergv"
+console.table([acoountId, accountName, accountPassword]);
 
-// this is null datatype and its standalone datatype 
-let tamprature = null
-
-//thiw is undifind value where we are not using any variablle value " "
-let undifind;
-
-// this is bigint when we need to large number in our project
-
-// symbol also one of uniqe datatype this is use for uniqe when we use symbol
-
-
-
-
-/* 1 string 
-   2 number 
-   3 boolean
-   4 null
-   5 undifind
-   6 symbol
-   7 bigint */
